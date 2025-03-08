37dad3352f8cb68ec20335b930a6cf23ba9d65c0
DChristenson <denchris32@gmail.com>
2025-03-07 19:19:56 -0500

    mob movement and spawning of mobs

---
+++ main.gd
@@ -0,0 +1,16 @@
+extends Node
+
+@export var mob_scene: PackedScene
+
+func _on_mob_timer_timeout() -> void:
+	var mob = mob_scene.instantiate()
+	
+	var mob_spawn_loc = get_node("SpawnPath/SpawnLocation")
+	
+	mob_spawn_loc.progress_ratio = randf()
+	
+	var player_position = $Player.position
+	
+	mob.initialize(mob_spawn_loc.position, player_position)
+	
+	add_child(mob)
--- mob.gd
+++ mob.gd
@@ -6,3 +6,14 @@ extends CharacterBody3D
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
+
+func initialize(start_position, player_position):
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	rotate_y(randf_range(-PI / 4, PI / 4))
+	var random_speed = randi_range(min_speed, max_speed)
+	velocity = Vector3.FORWARD * random_speed
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
+
+func _on_visibility_notifier_screen_exited() -> void:
+	queue_free()
