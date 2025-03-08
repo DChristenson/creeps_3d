60fb41ce5366c4cc3e90b80d4fd06cb4bbfea531
DChristenson <denchris32@gmail.com>
2025-03-08 13:29:20 -0500

    retry after dying

--- main.gd
+++ main.gd
@@ -23,3 +23,8 @@ func _on_player_hit() -> void:
 
 func _ready():
 	$UserInterface/Retry.hide()
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		# This restarts the current scene.
+		get_tree().reload_current_scene()
